b5e4a603ebd95368725fa671bbe3829a352bf515
Ly Nguyen <Ly900@aol.com>
2016-08-16 15:08:14 -0400

    Create main Battleship class and first set of game instructions.

---
+++ Battleship.java
@@ -0,0 +1,11 @@
+class Battleship {
+
+  public static void main(String [] args) {
+
+    System.out.println("Let's play Battleship!");
+    System.out.println("The ship stretches across 3 cells in a row with cells 0-7.");
+    System.out.println("Make guesses one cell at a time to try to sink the ship!");
+
+  }
+
+}
