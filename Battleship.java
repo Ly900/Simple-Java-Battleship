8357d20d1098ede1f7baa8c5f051ad19e408db31
Ly Nguyen <Ly900@aol.com>
2016-08-16 17:41:51 -0400

    Move creation of location cells to main method; Battleship has method to check the user's first guess

--- Battleship.java
+++ Battleship.java
@@ -4,18 +4,25 @@ class Battleship {
 
   private int[] locationCells;
 
-  // Randomly chooses an integer between 0-4 and creates three consecutive cells for battleship
-  public void setLocationCells() {
-    int firstCell = (int) (Math.random() * 6);
-    int[] locationCellsArray = new int[] {firstCell, firstCell + 1, firstCell + 2};
-    String stringArray = Arrays.toString(locationCellsArray);
-    System.out.println(stringArray);
+  public void setLocationCells(int[] loc) {
+    locationCells = loc;
   }
 
   public void checkGuess() {
     String userGuessString = System.console().readLine();
     int userGuess = Integer.parseInt(userGuessString);
     System.out.println("Your guess was " + userGuess);
+
+    for (int cell : locationCells) {
+      if (userGuess == cell) {
+        System.out.println("Hit!");
+        break;
+      } else {
+        System.out.println("Miss.");
+        break;
+      }
+    }
+
   }
 
 }
--- BattleshipGame.java
+++ BattleshipGame.java
@@ -10,7 +10,14 @@ class BattleshipGame {
     System.out.println("Make guesses one cell at a time to try to sink the ship!");
 
     Battleship battleship = new Battleship();
-    battleship.setLocationCells();
+    boolean isSunk = false;
+
+    // Randomly chooses an integer between 0-4 and creates three consecutive cells for battleship
+    int firstCell = (int) (Math.random() * 6);
+    int[] locationCellsArray = new int[] {firstCell, firstCell + 1, firstCell + 2};
+    battleship.setLocationCells(locationCellsArray);
+    String stringArray = Arrays.toString(locationCellsArray);
+    System.out.println("Random, secret location of battleship: " + stringArray);
 
     System.out.println("The battleship has been placed.");
     System.out.println("What is your first guess?");
